8b0fd24beb63c52c1b13f72e409bd104f232dbf8
Grigoriy Pridatkn <pridatkin@gmail.com>
2024-05-10 13:20:22 +0300

    correct the 2nd task in the lesson about lists

--- 08_lists/exercise2.py
+++ 08_lists/exercise2.py
@@ -1,5 +1,5 @@
 n = int(input("Enter the number of values: "))
-array = list(map(int, input().split()))
+array = list(map(int, input().split()))[:n]
 print("Source array:\t", array)
 array.insert(0, array.pop())
 print("Modified array\t", array)
