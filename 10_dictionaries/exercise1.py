563c00c58fa05b2aadfb111ef1f821fbaffda19d
Grigoriy Pridatkn <pridatkin@gmail.com>
2024-05-15 12:51:52 +0300

    add a solution to the task on the 10th lesson

---
+++ 10_dictionaries/exercise1.py
@@ -0,0 +1,19 @@
+pets = {}
+name = input("Введите имя питомца: ")
+kind =input("Введите вид питомца: ")
+age = int(input("Введите возраст питомца: "))
+owner = input("Введите имя хозяина питомца: ")
+
+pets[name] = {
+	'kind': kind,
+	'age': age,
+	'owner': owner
+}
+
+year = 'лет'
+if pets[name]['age'] % 10 == 1 and age % 100 != 11:
+	year = 'год'
+elif 1 < age % 10 < 5 and age != 12 and age != 13 and age != 14:
+	year = 'года'
+
+print(f"Это {pets[name]['kind']} по кличке: {name}. Возраст питомца: {pets[name]['age']} {year}. Имя владельца: {pets[name]['owner']}.")
---
+++ 10_dictionaries/exercise2.py
@@ -0,0 +1,9 @@
+my_dict = {}
+
+for i in range(10, -6, -1):
+	my_dict[i] = i ** i
+
+for i in range(10, -6, -1):
+	print(f"my_dict[{i}] = {my_dict[i]}")
+
+#print(my_dict)
